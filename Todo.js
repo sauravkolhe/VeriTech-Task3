c5d5be09c8536fb36d15089e80eb79bcea6f9875
Saurav Kolhe <76933852+sauravkolhe@users.noreply.github.com>
2024-04-05 11:26:46 +0530

    Add files via upload

---
+++ Todo.js
@@ -0,0 +1,43 @@
+let todoList = [
+  {
+    item:'Saurav Love',
+    dueDate: '22/12/2023'
+  },
+  {
+    item:'Dr.Prachi',
+    dueDate:'25/12/2023'
+  }];
+displayItems();
+
+function addTodo() {
+let inputElement = document.querySelector
+('#todo-input');
+let dateElement = document.querySelector
+('#todo-date');
+let todoItem = inputElement.value;
+let todoDate = dateElement.value
+
+todoList.push({item: todoItem,dueDate: todoDate});
+inputElement.value = '';
+
+  displayItems();
+
+}
+
+function displayItems() {
+  let containerElement = document.querySelector
+  ('.todo-container');
+  
+  let newHtml = '';
+  for (let i=0; i<todoList.length;i++) {
+    let {item,dueDate} = todoList[i];
+  
+    newHtml +=`
+    <span>${item}</span>
+    <span>${dueDate}</span>
+      <button class='btn-delete' onclick="todoList.splice(${i}, 1);displayItems();">Delete</button>
+      `;
+  }
+      containerElement.innerHTML = newHtml;
+
+}
